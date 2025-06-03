0d087e3c500f4020f4f4612abec3eca58b873524
Deepak Maurya <deepakrmaurya@outlook.com>
2025-06-03 17:40:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-amber-400">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
